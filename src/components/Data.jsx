cba39d7db295958722db8fdc6de7e70cb6840ee9
mibrahim234 <mohamm1417@gmail.com>
2022-03-03 23:32:09 -0500

    new component data for react questions

---
+++ src/components/Data.jsx
@@ -0,0 +1,70 @@
+// Page for the react questions and answers 
+
+// const data = [
+//     {
+//       id: 1,
+//       question: "What is a component?",
+//       answers: [
+//         {
+//           text: "phone",
+//           correct: false,
+//         },
+//         {
+//         text: "a piece of code",
+//         correct: true, 
+//         },
+//         {
+//           text: "cake",
+//           correct: false,
+//         },
+//         {
+//           text: "react is a component based library",
+//           correct: false, 
+//         }
+//       ]
+//     },
+//     {
+//       id: 2,
+//       question: "What is a computer?",
+//       answers: [
+//         {
+//           text: "phone",
+//           correct: false,
+//         },
+//         {
+//         text: "a piece of code",
+//         correct: true, 
+//         },
+//         {
+//           text: "cake",
+//           correct: false,
+//         },
+//         {
+//           text: "react is a component based library",
+//           correct: false, 
+//         }
+//       ]
+//     },
+//     {
+//       id: 3,
+//       question: "What is a phone?",
+//       answers: [
+//         {
+//           text: "phone",
+//           correct: false,
+//         },
+//         {
+//         text: "a piece of code",
+//         correct: true, 
+//         },
+//         {
+//           text: "cake",
+//           correct: false,
+//         },
+//         {
+//           text: "react is a component based library",
+//           correct: false, 
+//         }
+//       ]
+//     }
+//   ]
